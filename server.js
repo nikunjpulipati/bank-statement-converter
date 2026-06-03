f03c0edd75957d457d9ad8d924d857f55cad8ea5
Nikunj Pulipati <nikunjpulipati@Nikunjs-MacBook-Air.local>
2026-06-03 21:24:19 +0530

    Fix OAuth callback URL

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://bank-statement-converter-i2vk.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ googleId: profile.id });
   if (!user) {
