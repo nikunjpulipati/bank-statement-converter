bea576f54d1964788ac0fa5cc78739381a3df18d
Nikunj Pulipati <nikunjpulipati@Nikunjs-MacBook-Air.local>
2026-06-05 07:59:00 +0530

    Fix syntax error in Google strategy

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: 'https://bank-statement-converter-i2vk.onrender.com/auth/google/callback'
-}, async (accessToken, refreshToken, profile, done) => { try {
+}, async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ googleId: profile.id });
   if (!user) {
     user = await User.create({ googleId: profile.id, name: profile.displayName, email: profile.emails[0].value, photo: profile.photos[0].value });
